ac7f0d9772bbda647c99c9c5c8b1720ec4304d0d
Anmol Kumar <at4141anmol@gmail.com>
2026-05-11 22:47:42 +0530

    Pusing all old Work SASS APP Fusion Ai

---
+++ collge-project_AiFusion/client/src/pages/RemoveObject.jsx
@@ -0,0 +1,127 @@
+import { Scissors, Sparkles } from "lucide-react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const RemoveObject = () => {
+  const [input, setInput] = useState("");
+  const [object, setObject] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      if (object.split(" ").length > 1) {
+        return toast("please enter only one object name");
+      }
+
+      const formData = new FormData();
+      formData.append("image", input);
+      formData.append("object", object);
+
+      const { data } = await axios.post(
+        "/api/ai/remove-image-object",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left column */}
+      <div className="min-w-[300px]">
+        <form
+          onSubmit={onSubmitHandler}
+          className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+        >
+          <div className="flex items-center gap-2 mb-4">
+            <Sparkles className="w-6 h-6 text-[#FF4938]" />
+            <h2 className="text-xl font-semibold">Object Removal</h2>
+          </div>
+
+          <p className="mt-6 text-sm font-medium">Upload Images</p>
+          <input
+            type="file"
+            onChange={(e) => setInput(e.target.files[0])}
+            className="w-full p-2 mt-2 px-3 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+            required
+            accept="image/*"
+          />
+
+          <p className="mt-6 text-sm font-medium">
+            Describe object name to remove
+          </p>
+
+          <textarea
+            rows={4}
+            value={object}
+            onChange={(e) => setObject(e.target.value)}
+            className="w-full p-2 mt-2 outline-none text-sm rounded-md border border-gray-300"
+            placeholder="e.g., watch or spoon, only single object name"
+            required
+          />
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 transition"
+          >
+            {loading ? (
+              <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin "></span>
+            ) : (
+              <Scissors className="w-5 h-5" />
+            )}
+            Remove Object
+          </button>
+        </form>
+      </div>
+
+      {/* Right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-[24rem]">
+        <div className="flex items-center gap-3 mb-4">
+          <Scissors className="w-5 h-5 text-[#4A7AFF]" />
+          <h2 className="text-xl font-semibold">Processed Image</h2>
+        </div>
+
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <Scissors className="w-9 h-9" />
+              <p>Upload an image and click "Remove Object" to get started</p>
+            </div>
+          </div>
+        ) : (
+            <div className="mt-3 h-full">
+              <img src={content} alt="" className="w-full h-full" />
+            </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RemoveObject;
---
+++ collge-project_AiFusion/client/src/pages/ReviewResume.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { FileText, FileTextIcon, Scissors, Sparkles } from "lucide-react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const ReviewResume = () => {
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append("resume", input);
+
+      const { data } = await axios.post("/api/ai/resume-review", formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left column */}
+      <div className="min-w-[300px]">
+        <form
+          onSubmit={onSubmitHandler}
+          className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+        >
+          <div className="flex items-center gap-2 mb-4">
+            <Sparkles className="w-6 h-6 text-[#00DA83]" />
+            <h2 className="text-xl font-semibold">Resume Review</h2>
+          </div>
+
+          <p className="mt-6 text-sm font-medium">Upload Resume</p>
+          <input
+            type="file"
+            onChange={(e) => setInput(e.target.files[0])}
+            className="w-full p-2 mt-2 px-3 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+            required
+            accept="application/pdf"
+          />
+
+          <p className="mt-1 text-xs text-gray-500 font-medium">
+            Supports pdf resume only
+          </p>
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 transition"
+          >
+            {loading ? (
+              <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin "></span>
+            ) : (
+              <FileTextIcon className="w-5 h-5" />
+            )}
+            Review Resume
+          </button>
+        </form>
+      </div>
+
+      {/* Right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-[24rem]">
+        <div className="flex items-center gap-3 mb-4">
+          <FileText className="w-5 h-5 text-[#00DA83]" />
+          <h2 className="text-xl font-semibold">Analysis Result</h2>
+        </div>
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <FileText className="w-9 h-9" />
+              <p>Upload an Resume and click "Review Resume" to get started</p>
+            </div>
+          </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ReviewResume;
